6681f70d1e15dc891aea0c231fa846b47499b680
Akash Sarker <akashsarker2478@gmail.com>
2026-07-08 18:44:47 +0600

    database connect and users and issue table create

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,66 @@
-import express from 'express';
-const app = express()
-const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+import express, { type Application, type Request, type Response } from 'express';
+import {Pool} from "pg"
+import dotenv from "dotenv"
+
+dotenv.config();
+const app :Application = express()
+
+const port =process.env.PORT|| 5000;
+
+app.use(express.json())
+
+const pool =  new Pool({
+
+    connectionString:process.env.DB_URL
+})
+
+const initDB = async()=>{
+    try {
+        await pool.query(`
+        
+        CREATE TABLE IF NOT EXISTS users(
+            
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(100) NOT NULL,
+          email VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          role VARCHAR(50) DEFAULT 'contributor',
+          created_at TIMESTAMP DEFAULT NOW(),
+          updated_at TIMESTAMP DEFAULT NOW()
+        
+        )
+        
+        `);
+
+        await pool.query(`
+      CREATE TABLE IF NOT EXISTS issues (
+          id SERIAL PRIMARY KEY,
+          title VARCHAR(150) NOT NULL,
+          description TEXT NOT NULL,
+          type VARCHAR(50) NOT NULL,
+          status VARCHAR(50) DEFAULT 'open',
+          reporter_id INT NOT NULL,
+          created_at TIMESTAMP DEFAULT NOW(),
+          updated_at TIMESTAMP DEFAULT NOW()
+      );
+    `);
+
+    console.log("Database connected successfully!!")
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+initDB()
+
+app.get('/', (req :Request, res:Response) => {
+  res.status(200).json({
+    message:"express server",
+    "author":"akash sarker"
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`devpulse running on port ${port}`)
 })
